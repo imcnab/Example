ca02abbc7ba0973aebdd4f0325f43218e34d87f6
Ian McNab <imcnab@me.com>
2019-11-23 15:04:36 +1100

    Sound and String demos

---
+++ src/Chapter02/StrDemo.java
@@ -0,0 +1,9 @@
+package Chapter02;
+
+public class StrDemo {
+    public static void main(String[] args) {
+        System.out.println("First line\nSecond Line");
+        System.out.println("A\tB\tC");
+        System.out.println("D\tE\tF");
+    }
+}
