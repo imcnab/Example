0fc33a2d436a89f34c44c078f60020c2e0bed30c
Ian McNab <imcnab@me.com>
2019-11-23 13:07:23 +1100

    Last 3 examples of chapter 1 added

---
+++ src/GalToLitTable.java
@@ -0,0 +1,28 @@
+/*
+    Try This 1-2
+
+    This program displays a conversion
+    table of gallons to litres.
+
+    Call this program "GalToLitTable.java".
+ */
+
+public class GalToLitTable {
+    public static void main(String[] args) {
+        double gallons, litres;
+        int counter;
+
+        counter = 0;
+        for (gallons = 1; gallons <= 100 ; gallons++) {
+            litres = gallons * 3.7854;  // convert to litres
+            System.out.println(gallons + " gallons is " + litres + " litres.");
+            counter++;
+
+            // every 10th lines, print a blank line
+            if (counter==10) {
+                System.out.println();
+                counter = 0;    // reset the line counter
+            }
+        }
+    }
+}
