497d26f3371779c19f8efb06cb01a6723a4968a4
Ian McNab <imcnab@me.com>
2019-11-23 11:12:36 +1100

    Initial commit

---
+++ src/Example.java
@@ -0,0 +1,13 @@
+/*
+    This is a simple Java program.
+
+    Call this file Example.java
+
+ */
+
+public class Example {
+    // A java program begins with a call to main()
+    public static void main(String[] args) {
+        System.out.println("Java drives the Web.");
+    }
+}
