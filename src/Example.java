2632e555a3c026eb7ab8c0e0f7bfec057999e404
Ian McNab <imcnab@me.com>
2019-11-23 12:18:49 +1100

    examples 2 and 3 added

--- src/Example.java
+++ src/Example.java
@@ -8,6 +8,6 @@
 public class Example {
     // A java program begins with a call to main()
     public static void main(String[] args) {
-        System.out.println("Java drives the Web.");
+        System.out.println("Java drives the Web. " + args[0]);
     }
 }
